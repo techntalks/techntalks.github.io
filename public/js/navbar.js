a2e1209d70eab1067943f4b889779e643dd20a12
Jorge Julián Acero Lomelí <julianacero98@gmail.com>
2021-03-30 21:09:31 -0600

    home page changed

--- public/js/navbar.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector('.header')
-
-window.onscroll = () => {
-  if (window.scrollY > 1) {
-    header.classList.add('header__shadow')
-  } else {
-    header.classList.remove('header__shadow')
-  }
-}
